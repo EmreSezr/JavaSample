bf72b6801188527475d0f9236c8b78170ef50ed5
EmreSezr <101684245+EmreSezr@users.noreply.github.com>
2023-01-28 17:29:03 +0300

    Queue Class Sample

---
+++ QueueClass.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Bank {
+    public static void main(String[] args) {
+        // Queue yapısı oluşturalım
+      /*Java'da bir banka senaryosunda müşterilerin işlemlerini gerçekleştirmek için kuyruk yapısı*/
+        Queue<String> customers = new LinkedList<>();
+
+        // Müşteriler bankaya gelir
+        customers.add("Ahmet");
+        customers.add("Mehmet");
+        customers.add("Ali");
+        customers.add("Osman");
+
+        System.out.println("Bekleyen müşteriler: " + customers); // [Ahmet, Mehmet, Ali, Osman]
+        System.out.println("İlk müşteri: " + customers.peek()); // "Ahmet"
+        
+        // Banka çalışanları işlemleri yapar
+        customers.remove();
+        customers.remove();
+        customers.remove();
+        customers.remove();
+
+        System.out.println("Bekleyen müşteriler: " + customers); // []
