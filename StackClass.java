54f6e1ac8ee6a470c4e29479bb570baa10888bf4
EmreSezr <101684245+EmreSezr@users.noreply.github.com>
2023-01-28 17:24:35 +0300

    Stack Class Sample

---
+++ StackClass.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+public class Exam {
+    public static void main(String[] args) {
+        // Stack yapısı oluşturalım
+      
+      /*Örnek olarak, bir sınavda sınavı alan öğrencilerin isimlerini bir Stack yapısında saklayalım.
+      Öğrenciler sınavı almaya başladıklarında, isimleri Stack'e eklenir. Öğrenciler sınavı bitirdiklerinde, isimleri Stack'den çıkarılır.*/
+      
+        Stack<String> students = new Stack<>();
+
+        // Öğrenciler sınava girer
+        students.push("Ahmet");
+        students.push("Mehmet");
+        students.push("Ali");
+        students.push("Osman");
+
+        // Sınav biter
+        System.out.println("Sınavı alan öğrenciler: " + students);
+        System.out.println("Son giren öğrenci: " + students.peek()); // "Osman"
+        
+        // Öğrenciler sınavı bitirir
+        students.pop();
+        students.pop();
+        students.pop();
+        students.pop();
+
+        System.out.println("Sınavı alan öğrenciler: " + students);  // []
+    }
+}
